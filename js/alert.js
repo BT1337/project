e1e49c95a00c6ee49ab98395516d0d39cf4f2ae5
Bair <benedig@rambler.ru>
2023-03-06 13:28:00 +0900

    first commit

---
+++ js/alert.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//const answers = [];
+//answers [0] = prompt("Как вас зовут?", "");
+//answers [1] = prompt("Ваша фамилия?", "");
+//answers [2] = prompt("Сколько вам лет?", "");
+
+
+//console.log(answers);
+
+const user = "Баир";
+
+alert(`Привет, ${user}`);
