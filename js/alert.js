b623ee42b702010068b445dd0bd554336dbd9643
Bair <benedig@rambler.ru>
2023-03-07 14:37:03 +0900

    create app for movies

--- js/alert.js
+++ js/alert.js
@@ -8,6 +8,26 @@
 
 //console.log(answers);
 
-const user = "Баир";
+//const user = "Баир";
 
-alert(`Привет, ${user}`);
+//alert(`Привет, ${user}`);
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
